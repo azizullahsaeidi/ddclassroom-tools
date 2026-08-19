885f7d4e9abbde504608468774d410891e259c89
Azizullah <azizullahsaeidi@gmail.com>
2026-08-19 22:22:25 +0430

    Create email templates for grade 7,8 and 9

---
+++ resources/views/emails/monthly-attendance/grade-7.blade.php
@@ -0,0 +1,41 @@
+<p>
+    Dear {{ $student->name }},
+</p>
+
+<p>
+    This is your Grade 7 monthly attendance
+    update for {{ $month->name ?? '' }}
+    {{ $log->year }}.
+</p>
+
+<p>
+    Your absence percentage was
+    <strong>
+        {{ number_format(
+            $log->absence_percentage,
+            2
+        ) }}%
+    </strong>.
+</p>
+
+<p>
+    Because your absence percentage was more
+    than 30%, you are not eligible for monthly
+    support for this month.
+</p>
+
+<p>
+    Support type:
+    <strong>
+        {{
+            $log->support_type === 'credit_card'
+                ? 'Credit Card'
+                : 'Cash'
+        }}
+    </strong>
+</p>
+
+<p>
+    Regards,<br>
+    DDClassroom Team
+</p>
